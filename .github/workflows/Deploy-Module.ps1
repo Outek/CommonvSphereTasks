9e580e27debe8b5efd29ab55fb5ac6a98da8efd2
Daniel Siegenthaler <daniel.siegenthaler.1@gmail.com>
2019-10-19 13:59:43 +0200

    ci

---
+++ .github/workflows/Deploy-Module.ps1
@@ -0,0 +1,26 @@
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+$releaseNotes = $env:RELEASE_NOTES
+$moduleVersion = ($env:RELEASE_VERSION) -replace "v",""
+Write-Host "ModuleVersion: $moduleVersion"
+
+$manifestPath = Resolve-Path -Path "*\CommonvSphereTasks.psd1"
+#Write-Host "Manifest Path: $manifestPath"
+
+Update-ModuleManifest -ReleaseNotes $releaseNotes -Path $manifestPath.Path -ModuleVersion $moduleVersion #-Verbose
+
+$moduleFilePath = Resolve-Path -Path "*\CommonvSphereTasks.psm1"
+#Write-Host "Module File Path: $moduleFilePath"
+
+$modulePath = Split-Path -Parent $moduleFilePath
+#Write-Host "Module Path: $modulePath"
+
+$nuGetApiKey = $env:PSGALLERY_TOKEN
+
+try{
+    Publish-Module -Path $modulePath -NuGetApiKey $nuGetApiKey -ErrorAction Stop -Force #-Debug
+    Write-Host "The CommonvSphereTasks Module Version $moduleVersion has been Published to the PowerShell Gallery!"
+}
+catch {
+    throw $_
+}
---
+++ .github/workflows/Invoke-PesterTests.ps1
@@ -0,0 +1,9 @@
+$root = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+$result = Invoke-Pester -OutputFile "$root\Slack.Tests.xml" -OutputFormat NUnitXML -PassThru
+$result | ConvertTo-Json -Depth 100 | Out-File -FilePath "$root\Slack.Tests.json"
+
+if ($result.failedCount -ne 0) { 
+    $x = $result.failedCount
+    Write-Error "Pester returned $x errors. Build aborted."
+}
